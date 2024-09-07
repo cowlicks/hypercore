f3706e8d1c3747ce521f0a8066ae80d1b57a2430
Blake Griffith <blake.a.griffith@gmail.com>
2024-09-07 00:24:25 -0400

    rm use of OnAppendEvent

--- src/replication/events.rs
+++ src/replication/events.rs
@@ -1,21 +1,10 @@
 //! events related to replication
-//!
-use crate::{AppendOutcome, HypercoreError};
+use crate::{common::BitfieldUpdate, AppendOutcome, DataBlock, HypercoreError};
 use tokio::sync::broadcast;
 
 static MAX_EVENT_QUEUE_CAPACITY: usize = 32;
 
-#[derive(Clone, Debug)]
 /// Event emeitted by on_append
-pub struct OnAppendEvent {
-    /// outcome of append
-    pub append_outcome: AppendOutcome,
-    /// startind block index of append
-    pub start: u64,
-    /// length of apppend (in blocks )
-    pub length: u64,
-}
-
 #[derive(Debug, Clone)]
 /// Emitted when [`Hypercore::get`] is called when the block is missing.
 pub struct OnGetEvent {
@@ -25,24 +14,63 @@ pub struct OnGetEvent {
     pub get_result: broadcast::Sender<()>,
 }
 
+/// When an upgrade is applied
+#[derive(Debug, Clone)]
+pub struct OnDataUpgradeEvent {}
+
+/// When an upgrade is applied
+/// rename it to onhave
+#[derive(Debug, Clone)]
+pub struct OnHaveEvent {
+    /// TODO
+    pub start: u64,
+    /// TODO
+    pub length: u64,
+    /// TODO
+    pub drop: bool,
+}
+
+impl From<&BitfieldUpdate> for OnHaveEvent {
+    fn from(
+        BitfieldUpdate {
+            start,
+            length,
+            drop,
+        }: &BitfieldUpdate,
+    ) -> Self {
+        OnHaveEvent {
+            start: *start,
+            length: *length,
+            drop: *drop,
+        }
+    }
+}
+
 #[derive(Debug, Clone)]
 /// Core events relative to the replicator
 pub enum EventMsg {
-    /// emmited when core.append happens
-    OnAppend(OnAppendEvent),
     /// emmited when core.get(i) happens for a missing block
     OnGet(OnGetEvent),
-}
-
-impl From<OnAppendEvent> for EventMsg {
-    fn from(value: OnAppendEvent) -> Self {
-        EventMsg::OnAppend(value)
-    }
+    /// emmitted when data.upgrade applied
+    OnDataUgrade(OnDataUpgradeEvent),
+    /// emmitted when core gets new blocks
+    OnHave(OnHaveEvent),
 }
 
 impl From<OnGetEvent> for EventMsg {
     fn from(value: OnGetEvent) -> Self {
         EventMsg::OnGet(value)
+    }
+}
+
+impl From<OnDataUpgradeEvent> for EventMsg {
+    fn from(value: OnDataUpgradeEvent) -> Self {
+        EventMsg::OnDataUgrade(value)
+    }
+}
+impl From<OnHaveEvent> for EventMsg {
+    fn from(value: OnHaveEvent) -> Self {
+        EventMsg::OnHave(value)
     }
 }
 
